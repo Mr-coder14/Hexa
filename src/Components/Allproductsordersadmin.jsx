93fe5af69722246aecfb36097b5c77ed72f0b525
kavi6265 <akavi6265@gmail.com>
2025-10-08 23:40:30 +0530

    Update Allproductsordersadmin.jsx

--- src/Components/Allproductsordersadmin.jsx
+++ src/Components/Allproductsordersadmin.jsx
@@ -108,7 +108,7 @@ function Allproductsordersadmin() {
   const getImageFilename = (imageId) => {
     const IMAGE_ID_MAPPING = {
         
-        "2131230840": "about_us.png",
+    "2131230840": "about_us.png",
     "2131230841": "afoursheet.png",
     "2131230842": "athreenote.png",
     "2131230843": "athreenotee.jpg",
